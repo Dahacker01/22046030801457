7dae96d611da2774d46d9c1d35489709264ab342
Dahacker01 <nhackin0@byu.edu>
2025-11-17 17:41:08 -0700

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -2,7 +2,7 @@
 $imageUrl = "https://elements-resized.envatousercontent.com/elements-video-cover-images/ef547aaf-3b49-461e-ab10-c9acb1519d19/video_preview/video_preview_0000.jpg?w=1200&h=630&cf_fit=crop&q=85&format=jpeg&s=37e0ba759e20a56e2cd7f1c90f652a60892836af854a4a24f54b1612872d7f35"
 
 # Local path to save the image
-$localPath = "$env:USERPROFILE\sony_cyberattack.jpg"
+$localPath = "$env:USERPROFILE\video_preview_0000.jpg"
 
 # Download the image
 Invoke-WebRequest -Uri $imageUrl -OutFile $localPath
